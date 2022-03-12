6c38d3c322c3ff3f1ad3135baa5a8e5e2cce32aa
Edouard Levasseur <lewardo23@gmail.com>
2022-03-12 14:53:32 +0000

    reordered includes

--- libml.h
+++ libml.h
@@ -1,10 +1,14 @@
 #pragma once
 
-#include "network.h"
-#include "corpus.h"
+#include "types.h"
+#include "utils.h"
 
 #include "actfunc.h"
 #include "lossfunc.h"
 
+#include "network.h"
+#include "corpus.h"
+
 #include "mlp.h"
 
+namespace ml {};
