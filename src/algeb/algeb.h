7ec62d388e3d7f10b5d41211305b80e758763e7d
Edouard Levasseur <lewardo23@gmail.com>
2022-03-18 12:48:25 +0000

    migration to operator overloads

---
+++ src/algeb/algeb.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+#include "types.h"
+
+
+std::vector<ml::types::flt> operator+(const std::vector<ml::types::flt>&, const ml::types::flt&);
+
+namespace ml::types {
+    
+}
