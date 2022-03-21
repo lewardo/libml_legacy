457797a0c4c645d3a606571cfce86282e3ce31f2
Edouard Levasseur <lewardo23@gmail.com>
2022-03-21 13:17:49 +0000

    library path headers

--- libml.h
+++ libml.h
@@ -1,17 +1,16 @@
 #pragma once
 
-#include "types.h"
-#include "utils.h"
+#include "libml/types.h"
+#include "libml/utils.h"
 
-#include "vector.h"
-#include "matrix.h"
+#include "libml/algeb.h"
 
-#include "actfunc.h"
-#include "lossfunc.h"
+#include "libml/actfunc.h"
+#include "libml/lossfunc.h"
 
-#include "network.h"
-#include "corpus.h"
+#include "libml/network.h"
+#include "libml/corpus.h"
 
-#include "mlp.h"
+#include "libml/mlp.h"
 
 namespace ml {};
