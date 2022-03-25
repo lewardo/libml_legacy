d2a84b8f4c3a7d47cccfa46bf2934f8f61cad607
Edouard Levasseur <lewardo23@gmail.com>
2022-03-25 12:40:05 +0000

    reader and writer classes

---
+++ src/utils/reader.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <vector>
+
+#include "types.h"
+#include "utils.h"
+
+using namespace ml::types;
+using namespace ml::utils;
+
+class ml::utils::reader {
+    public:
+        
+    
+    private:
+        std::ifstream _ofs;
+};
---
+++ src/utils/writer.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <vector>
+
+#include "types.h"
+#include "utils.h"
+
+using namespace ml::types;
+using namespace ml::utils;
+
+class ml::utils::writer {
+    public:
+        
+    
+    private:
+        std::ofstream _ofs;
+};
