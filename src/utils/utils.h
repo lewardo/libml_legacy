e6f1f855961298b265b4f2314b9e79a6264784fc
Edouard Levasseur <lewardo23@gmail.com>
2022-04-13 18:45:26 +0100

    iterator index calculation

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,15 +1,38 @@
 #pragma once
 
-#include <random>
-#include <concepts>
 #include <ranges>
-#include <cstdbool>
+#include <concepts>
+#include <tuple>
+#include <fstream>
+#include <typeinfo>
 
 #include "types.h"
 
 namespace ml::utils {
     using namespace ml::types;
     
-    class reader;
-    class writer;
+    template <typename T, typename vT = T::value_type, typename dT = T::difference_type, typename iT = T::const_iterator> requires std::ranges::range<T>
+    inline dT iterator_index(vT& element, T& container) {
+        return static_cast<dT>( static_cast<iT>(&element) - container.begin() );
+    };
+    
+    class reader {
+        public:
+            reader& operator>>(vector&) const;
+            reader& operator>>(matrix&) const;
+            
+            std::type_info& typeof_next() const;
+            
+        private:
+            std::ifstream _ifs;
+    };
+    
+    class writer {
+        public:
+            writer& operator<<(const vector&) const;
+            writer& operator<<(const matrix&) const;
+                        
+        private:
+            std::ofstream _ofs;
+    };
 };
