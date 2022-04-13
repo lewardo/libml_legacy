75acbf82a4fcfcf2042897cddcf2634f80024fae
Edouard Levasseur <lewardo23@gmail.com>
2022-04-13 18:43:33 +0100

    operators and transpose

--- src/algeb/matrix.cpp
+++ src/algeb/matrix.cpp
@@ -10,39 +10,16 @@
 #include "algeb.h"
 #include "utils.h"
 
-using namespace ml::types;
+using namespace ml::utils;
 
 namespace ml::types {
-    enum vector_class: uint32_t {
-        COL_VEC = 0,
-        ROW_VEC,
-    };
-
-    template <typename ...Args>
-    matrix repeat(const vector_class c, const uint32_t n, Args&&... args) {
-        vector v(std::forward<Args>(args)...);
-        matrix m(n, v);
-
-        if(c == COL_VEC) return transpose(m);
-        else return m;
-    }
-
-    template <typename ...Args>
-    matrix repeat(const vector_class c, const uint32_t n, std::initializer_list<flt> init, Args&&... args) {
-        vector v(std::forward<std::initializer_list<flt>> (init), std::forward<Args> (args)...);
-        matrix m(n, v);
-
-        if(c == COL_VEC) return transpose(m);
-        else return m;
-    }
-
     bool verify(const matrix& m) {
         const size_t size = m.front().size();
         
         return std::all_of(
-            std::execution::par,
+            std::execution::par_unseq,
             m.begin() + 1, m.end(), 
-            [&size](const vector& v) {
+            [&size](const vector& v) -> decltype(v.size()) {
                 return v.size();
             }
         );
@@ -54,16 +31,16 @@ namespace ml::types {
         matrix transposed(term.front().size(), vector(term.size()));
         
         std::for_each(
-            std::execution::par, 
+            std::execution::par_unseq, 
             term.begin(), term.end(),
             [&](const vector& row) {
-                typename matrix::difference_type row_n = c_cast<typename matrix::const_iterator>( &row ) - term.begin();
+                auto row_n = iterator_index(row, term);
                     
                 std::for_each(
-                    std::execution::par,
+                    std::execution::par_unseq,
                     row.begin(), row.end(),
                     [&](const flt& val) {
-                        transposed[c_cast<typename vector::const_iterator> (&val) - row.begin()][row_n] = val;
+                        transposed[iterator_index(val, row)][row_n] = val;
                     }
                 );
             }
@@ -71,257 +48,259 @@ namespace ml::types {
         
         return transposed;
     };
+
+    matrix operator+(const matrix& lterm, const flt& rterm) {
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        vector vec(lterm.front().size(), rterm);
+
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            vec.begin(), 
+            res.begin(), 
+            [](const vector& row, const flt& val) -> vector {
+                return row + val;
+            }
+        );
+
+        return res;
+    };
+
+    matrix operator+(const matrix& lterm, const matrix& rterm) {
+        if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
+
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        
+        std::transform(
+            std::execution::par_unseq,
+            lterm.begin(), lterm.end(),
+            rterm.begin(), 
+            res.begin(),
+            [](const vector& lrow, const vector& rrow) -> std::vector<flt> {
+                return lrow + rrow;
+            }
+        );
+
+        return res;
+    };
+
+    matrix& operator+=(matrix& lterm, const flt& rterm) {
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        vector vec(lterm.front().size(), rterm);
+
+        std::transform(
+            std::execution::par_unseq,
+            lterm.begin(), lterm.end(),
+            vec.begin(),
+            res.begin(),
+            [](const vector& row, const flt& val) -> vector {
+                return row + val;
+            }
+        );
+
+        lterm = std::move(res);
+
+        return lterm;
+    };
+
+    matrix& operator+=(matrix& lterm, const matrix& rterm) {
+        if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
+
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            rterm.begin(), 
+            res.begin(), 
+            [](const vector& lrow, const vector& rrow) -> vector {
+                return lrow + rrow;
+            }
+        );
+
+        lterm = std::move(res);
+
+        return lterm;
+    };
+
+    matrix operator-(const matrix& lterm, const flt& rterm) {
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        vector vec(lterm.front().size(), rterm);
+
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            vec.begin(), 
+            res.begin(), 
+            [](const vector& row, const flt& val) -> vector {
+                return row - val;
+            }
+        );
+
+        return res;
+    };
+
+    matrix operator-(const matrix& lterm, const matrix& rterm) {
+        if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
+
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        
+        std::transform(
+            std::execution::par_unseq,
+            lterm.begin(), lterm.end(),
+            rterm.begin(), 
+            res.begin(),
+            [](const vector& lrow, const vector& rrow) -> std::vector<flt> {
+                return lrow - rrow;
+            }
+        );
+
+        return res;
+    };
+
+    matrix& operator-=(matrix& lterm, const flt& rterm) {
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        vector vec(lterm.front().size(), rterm);
+
+        std::transform(
+            std::execution::par_unseq,
+            lterm.begin(), lterm.end(),
+            vec.begin(),
+            res.begin(),
+            [](const vector& row, const flt& val) -> vector {
+                return row - val;
+            }
+        );
+
+        lterm = std::move(res);
+
+        return lterm;
+    };
+
+    matrix& operator-=(matrix& lterm, const matrix& rterm) {
+        if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
+
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            rterm.begin(), 
+            res.begin(), 
+            [](const vector& lrow, const vector& rrow) -> vector {
+                return lrow - rrow;
+            }
+        );
+
+        lterm = std::move(res);
+
+        return lterm;
+    };
+
+    matrix operator*(const matrix& lterm, const flt& rterm) {
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        vector vec(lterm.front().size(), rterm);
+
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            vec.begin(), 
+            res.begin(), 
+            [](const vector& row, const flt& val) -> vector {
+                return row * val;
+            }
+        );
+
+        return res;
+    };
+
+    vector operator*(const matrix& lterm, const vector& rterm) {
+        if(lterm.front().size() != rterm.size()) throw std::length_error("matrix vector multipliciation size mismatch");
+        
+        matrix mat = ml::types::repeat(ROW_VEC, lterm.size(), rterm);
+        vector res(lterm.size());
+
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            mat.begin(), 
+            res.begin(), 
+            [](const vector& lrow, const vector& rrow) -> flt {
+                return lrow * rrow;
+            }
+        );
+
+        return res;
+    };
+
+    // matrix operator*(const matrix& lterm, const matrix& rterm) {
+    // 
+    // };
+
+    matrix operator&(const matrix& lterm, const flt& rterm) {
+        return lterm * rterm;
+    };
+
+    matrix operator&(const matrix& lterm, const matrix& rterm) {
+        if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator& matrix size mismatch");
+
+        matrix res(lterm.size());
+        
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            rterm.begin(), 
+            res.begin(), 
+            [](const vector& lrow, const vector& rrow) -> vector {
+                return lrow & rrow;
+            }
+        );
+
+        return res;
+    };
+
+    matrix operator*=(matrix& lterm, const flt& rterm) {
+        matrix res(lterm.size(), vector(lterm.front().size()));
+        vector vec(lterm.front().size(), rterm);
+
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            vec.begin(), 
+            res.begin(), 
+            [](const vector& row, const flt& val) -> vector {
+                return row * val;
+            }
+        );
+        
+        lterm = std::move(res);
+
+        return lterm;
+    };
+
+    matrix operator&=(matrix& lterm, const flt& rterm) {
+        return lterm *= rterm;
+    };
+
+    matrix operator&=(matrix& lterm, const matrix& rterm) {
+        if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator& matrix size mismatch");
+
+        matrix res(lterm.size());
+        
+        std::transform(
+            std::execution::par_unseq, 
+            lterm.begin(), lterm.end(), 
+            rterm.begin(), 
+            res.begin(), 
+            [](const vector& lrow, const vector& rrow) -> vector {
+                return lrow & rrow;
+            }
+        );
+        
+        lterm = std::move(res);
+
+        return lterm;
+    };
+    
+    // matrix& operator*=(const std::matrix<flt>& other);
 }
-
-matrix operator+(const matrix& lterm, const flt& rterm) {
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    vector vec(lterm.front().size(), rterm);
-
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        vec.begin(), 
-        res.begin(), 
-        [](const vector& row, const flt& val) -> vector {
-            return row + val;
-        }
-    );
-
-    return res;
-};
-
-matrix operator+(const matrix& lterm, const matrix& rterm) {
-    if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
-
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    
-    std::transform(
-        std::execution::par,
-        lterm.begin(), lterm.end(),
-        rterm.begin(), 
-        res.begin(),
-        [](const vector& lrow, const vector& rrow) -> std::vector<flt> {
-            return lrow + rrow;
-        }
-    );
-
-    return res;
-};
-
-matrix& operator+=(matrix& lterm, const flt& rterm) {
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    vector vec(lterm.front().size(), rterm);
-
-    std::transform(
-        std::execution::par,
-        lterm.begin(), lterm.end(),
-        vec.begin(),
-        res.begin(),
-        [](const vector& row, const flt& val) -> vector {
-            return row + val;
-        }
-    );
-
-    lterm = std::move(res);
-
-    return lterm;
-};
-
-matrix& operator+=(matrix& lterm, const matrix& rterm) {
-    if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
-
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        rterm.begin(), 
-        res.begin(), 
-        [](const vector& lrow, const vector& rrow) -> vector {
-            return lrow + rrow;
-        }
-    );
-
-    lterm = std::move(res);
-
-    return lterm;
-};
-
-matrix operator-(const matrix& lterm, const flt& rterm) {
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    vector vec(lterm.front().size(), rterm);
-
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        vec.begin(), 
-        res.begin(), 
-        [](const vector& row, const flt& val) -> vector {
-            return row - val;
-        }
-    );
-
-    return res;
-};
-
-matrix operator-(const matrix& lterm, const matrix& rterm) {
-    if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
-
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    
-    std::transform(
-        std::execution::par,
-        lterm.begin(), lterm.end(),
-        rterm.begin(), 
-        res.begin(),
-        [](const vector& lrow, const vector& rrow) -> std::vector<flt> {
-            return lrow - rrow;
-        }
-    );
-
-    return res;
-};
-
-matrix& operator-=(matrix& lterm, const flt& rterm) {
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    vector vec(lterm.front().size(), rterm);
-
-    std::transform(
-        std::execution::par,
-        lterm.begin(), lterm.end(),
-        vec.begin(),
-        res.begin(),
-        [](const vector& row, const flt& val) -> vector {
-            return row - val;
-        }
-    );
-
-    lterm = std::move(res);
-
-    return lterm;
-};
-
-matrix& operator-=(matrix& lterm, const matrix& rterm) {
-    if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator+ matrix size mismatch");
-
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        rterm.begin(), 
-        res.begin(), 
-        [](const vector& lrow, const vector& rrow) -> vector {
-            return lrow - rrow;
-        }
-    );
-
-    lterm = std::move(res);
-
-    return lterm;
-};
-
-matrix operator*(const matrix& lterm, const flt& rterm) {
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    vector vec(lterm.front().size(), rterm);
-
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        vec.begin(), 
-        res.begin(), 
-        [](const vector& row, const flt& val) -> vector {
-            return row * val;
-        }
-    );
-
-    return res;
-};
-
-vector operator*(const matrix& lterm, const vector& rterm) {
-    matrix mat = ml::types::repeat(ROW_VEC, lterm.size(), rterm);
-    vector res(lterm.size());
-
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        mat.begin(), 
-        res.begin(), 
-        [](const vector& lrow, const vector& rrow) -> flt {
-            return lrow * rrow;
-        }
-    );
-
-    return res;
-};
-
-// matrix operator*(const matrix& lterm, const matrix& rterm) {
-// 
-// };
-
-matrix operator&(const matrix& lterm, const flt& rterm) {
-    return operator*(std::forward<const matrix&>(lterm), std::forward<const flt&>(rterm));
-};
-
-matrix operator&(const matrix& lterm, const matrix& rterm) {
-    if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator& matrix size mismatch");
-
-    matrix res(lterm.size());
-    
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        rterm.begin(), 
-        res.begin(), 
-        [](const vector& lrow, const vector& rrow) -> vector {
-            return lrow & rrow;
-        }
-    );
-
-    return res;
-};
-
-matrix operator*=(matrix& lterm, const flt& rterm) {
-    matrix res(lterm.size(), vector(lterm.front().size()));
-    vector vec(lterm.front().size(), rterm);
-
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        vec.begin(), 
-        res.begin(), 
-        [](const vector& row, const flt& val) -> vector {
-            return row * val;
-        }
-    );
-    
-    lterm = std::move(res);
-
-    return lterm;
-};
-
-matrix operator&=(matrix& lterm, const flt& rterm) {
-    return operator*=(std::forward<matrix&>(lterm), std::forward<const flt&>(rterm));
-};
-
-matrix operator&=(matrix& lterm, const matrix& rterm) {
-    if(lterm.size() != rterm.size() || lterm.front().size() != rterm.front().size()) throw std::length_error("operator& matrix size mismatch");
-
-    matrix res(lterm.size());
-    
-    std::transform(
-        std::execution::par, 
-        lterm.begin(), lterm.end(), 
-        rterm.begin(), 
-        res.begin(), 
-        [](const vector& lrow, const vector& rrow) -> vector {
-            return lrow & rrow;
-        }
-    );
-    
-    lterm = std::move(res);
-
-    return lterm;
-};
-
-// matrix& operator*=(const std::matrix<flt>& other);
