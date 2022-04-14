54aa0752151ea497cde7d76a09b0f22f05a83444
Edouard Levasseur <lewardo23@gmail.com>
2022-04-14 18:22:32 +0100

    removed superfluous main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-#include "libml.h"
-
-int32_t main(int32_t argc, const char * argv[]) {
-    printf("%f\n", lossfunc::accumulate({ 0, 0, 0.5, 1 }, { 1, 0, 1, 1 }, lossfunc::MeanSquared.f_x));
- 
-    return 0;
-}
