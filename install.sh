289261f0dc393722a938d9996cac12c45b945a7a
Edouard Levasseur <lewardo23@gmail.com>
2022-04-15 14:49:03 +0100

    involved dependencies

--- install.sh
+++ install.sh
@@ -2,16 +2,19 @@
 
 if [[ $(command -v git) ]] && [[ $(command -v sudo) ]] && [[ $(command -v make) ]]
 then
-    export DIR = ${TMPDIR:-/var/tmp/}
+    DIR=${TMPDIR:-/var/tmp/}/libml
     
     mkdir -p $DIR
     
     git clone https://github.com/lewardo/libml.git $DIR
     
     cd $DIR
-    sudo make install
+    make install PREFIX=/usr/local
     
     rm -rf $DIR
 else
-    echo "pls install git and retry"
+    echo "pls install dependencies and retry"
+    [ ! $(command -v git) ] && echo -e "\t✘ git" || echo -e "\t✔ git"
+    [ ! $(command -v sudo) ] && echo -e "\t✘ sudo" || echo -e "\t✔ sudo"
+    [ ! $(command -v make) ] && echo -e "\t✘ make" || echo -e "\t✔ make"
 fi
