68e843819f6a53dc921fbc225be98e9bb03361e6
Edouard Levasseur <lewardo23@gmail.com>
2022-04-17 13:27:51 +0100

    namespace detail idiom

--- src/utils/actfunc.cpp
+++ src/utils/actfunc.cpp
@@ -2,67 +2,66 @@
 
 #include "actfunc.h"
 
-using namespace ml::internal::types;
-using namespace ml::actf;
+namespace ml::actf::detail {
+
+    /*
+     *  sigmoid activation
+     */
+
+    value_type sigmoid = {
+        [](flt x) -> flt {
+            return 1.0f / (1.0f + std::exp(-1.0f * x));
+        },
+
+        [](flt x) -> flt {
+            return x * (1.0f - x);
+        }
+    };
 
 
-/*
- *  sigmoid activation
- */
+    /*
+     *  hyperbolic tangent activation
+     */
 
-value_type ml::actf::sigmoid = {
-    [](flt x) -> flt {
-        return 1.0f / (1.0f + std::exp(-1.0f * x));
-    },
+    value_type tanh = {
+        [] (flt x) -> flt {
+            return std::tanh(x);
+        },
 
-    [](flt x) -> flt {
-        return x * (1.0f - x);
-    }
-};
-
-
-/*
- *  hyperbolic tangent activation
- */
-
-value_type ml::actf::tanh = {
-    [] (flt x) -> flt {
-        return std::tanh(x);
-    },
-
-    [] (flt x) -> flt {
-        return 1.0f - x * x;
-    }
-};
+        [] (flt x) -> flt {
+            return 1.0f - x * x;
+        }
+    };
 
 
-/*
- *  Rectified Linear Unit activation
- */
+    /*
+     *  Rectified Linear Unit activation
+     */
 
-value_type ml::actf::relu = {
-    [] (flt x) -> flt {
-        if(x > 0.0f) return x;
-        else return 0.0f;
-    },
+    value_type relu = {
+        [] (flt x) -> flt {
+            if(x > 0.0f) return x;
+            else return 0.0f;
+        },
 
-    [] (flt x) -> flt {
-        if(x > 0.0f) return 1.0f;
-        else return 0.0f;
-    }
-};
+        [] (flt x) -> flt {
+            if(x > 0.0f) return 1.0f;
+            else return 0.0f;
+        }
+    };
 
 
-/*
- *  Linear activation
- */
+    /*
+     *  Linear activation
+     */
 
-value_type ml::actf::linear = {
-    [] (flt x) -> flt {
-        return x;
-    },
+    value_type linear = {
+        [] (flt x) -> flt {
+            return x;
+        },
 
-    [] (flt) -> flt {
-        return 1.0f;
-    }
-};
+        [] (flt) -> flt {
+            return 1.0f;
+        }
+    };
+}
--- src/utils/lossfunc.cpp
+++ src/utils/lossfunc.cpp
@@ -8,67 +8,67 @@
 #include "algeb.h"
 #include "lossfunc.h"
 
-using namespace ml::internal::types;
-using namespace ml::lossf;
+namespace ml::lossf::detail {
 
-/*
- *  Accumulator function to simplify error calculation
- */
+    /*
+     *  Accumulator function to simplify error calculation
+     */
 
-[[ maybe_unused ]]
-flt ml::lossf::accumulate(const vector &a, const vector &b, std::function<flt (flt, flt)> &f) {        
-    if(a.size() != b.size()) throw std::invalid_argument("accumulate vector size mismatch");  // throw if mismatched sizes
+    [[ maybe_unused ]]
+    flt accumulate(const vector &a, const vector &b, std::function<flt (flt, flt)> &f) {        
+        if(a.size() != b.size()) throw std::invalid_argument("accumulate vector size mismatch");  // throw if mismatched sizes
 
-    return std::transform_reduce(std::execution::par_unseq, a.begin(), a.end(), b.begin(), 0.0f, std::plus<flt>(), f);
-};
-
-
-/*
- *  Mean Squared Error (L2)
- */
-
-value_type ml::lossf::mse = {
-    [](flt x, flt t) -> flt {
-        flt d = t - x;
-        return 0.5f * d * d;
-    },
-
-    [](flt x, flt t) -> flt {
-        return x - t;
-    },
-};
+        return std::transform_reduce(std::execution::par_unseq, a.begin(), a.end(), b.begin(), 0.0f, std::plus<flt>(), f);
+    };
 
 
-/*
- *  Mean Absolute Error (L1)
- */
+    /*
+     *  Mean Squared Error (L2)
+     */
 
-value_type ml::lossf::mae = {
-    [](flt x, flt t) -> flt {
-        return std::abs(t - x);
-    },
+    value_type mse = {
+        [](flt x, flt t) -> flt {
+            flt d = t - x;
+            return 0.5f * d * d;
+        },
 
-    [](flt x, flt t) -> flt {
-        if(x < t) return -1.0f;
-        else if(x > t) return 1.0f;
-
-        return 0.0f;
-    },
-};
+        [](flt x, flt t) -> flt {
+            return x - t;
+        },
+    };
 
 
-/*
- *  Cross Entropy or Log Loss Error
- */
+    /*
+     *  Mean Absolute Error (L1)
+     */
 
-value_type ml::lossf::xee = {
-    [](flt x, flt t) -> flt {
-        if(t == 1.0f) return -1.0f * std::log(x);
-        return -1.0f * std::log(1.0f - x);
-    },
+    value_type mae = {
+        [](flt x, flt t) -> flt {
+            return std::abs(t - x);
+        },
 
-    [](flt x, flt t) -> flt {
-        if(t == 1.0f) return -1.0f / x;
-        return -1.0f / (1.0f - t);
-    },
-};
+        [](flt x, flt t) -> flt {
+            if(x < t) return -1.0f;
+            else if(x > t) return 1.0f;
+
+            return 0.0f;
+        },
+    };
+
+
+    /*
+     *  Cross Entropy or Log Loss Error
+     */
+
+    value_type xee = {
+        [](flt x, flt t) -> flt {
+            if(t == 1.0f) return -1.0f * std::log(x);
+            return -1.0f * std::log(1.0f - x);
+        },
+
+        [](flt x, flt t) -> flt {
+            if(t == 1.0f) return -1.0f / x;
+            return -1.0f / (1.0f - t);
+        },
+    };
+}
