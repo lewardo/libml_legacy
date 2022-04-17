ca638260ce927705453c287710011ef0e7f08473
Edouard Levasseur <lewardo23@gmail.com>
2022-04-17 13:28:26 +0100

    added comments

--- include/types.h
+++ include/types.h
@@ -4,17 +4,43 @@
 #include <vector>
 #include <array>
 
+
+/*
+ *  c++ style casting function for vanilla c cast; i.e. c_cast<to>(from)
+ */
+
 template <typename T>
 T c_cast(auto&& o) {
     return ((T) (o));
 };
 
+
+/*
+ *  namespace types
+ */
+
 namespace ml::internal::types {
-    using flt = float;  // or a double if you want a global change
     
+    /*
+     *  library-wide decimal precision, for example 
+     */
+     
+    using flt = float;
+    
+    
+    /*
+     *  vector and matrix type aliases
+     */
+     
     using vector = std::vector<flt>;
-    using matrix = std::vector<vector>;
-        
-    template <std::size_t N, typename T = vector>
+    using matrix = std::vector<std::vector<flt>>;
+    
+    
+    /*
+     *  time series of fixed length as array
+     */
+     
+    template <std::size_t N, typename T>
     using series = std::array<T, N>;
+    
 };
