4b75c3bf73a2cf531f0914c728937845bf1332a6
Edouard Levasseur <lewardo23@gmail.com>
2022-04-18 09:48:51 +0100

    implemented test funtions

--- src/impl/mlp.cpp
+++ src/impl/mlp.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 
 #include "types.h"
 #include "utils.h"
@@ -10,28 +10,37 @@ using namespace ml::internal::types;
 using namespace ml::internal::utils;
 
 namespace ml::networks::detail {
-    flt mlp_impl::propagate_values(const vector&, vector&) {
+    mlp_impl::mlp_impl() {
+        std::printf("constructing mlp impl\n");
+    }
+    
+    flt mlp_impl::propagate_values(const vector& a, vector& b) {
+        std::printf("propagate_values, %ld %ld\n", a.size(), b.size());
         
         return default_value<float>::value;
     };
 
-    int mlp_impl::calculate_update(const vector&, vector&) {
+    int mlp_impl::calculate_update(const vector& a, vector& b) {
+        std::printf("calculate_update, %ld %ld\n", a.size(), b.size());
 
         return default_value<int>::value;
     };
 
     int mlp_impl::execute_update() {
+        std::printf("execute_update\n");
 
         return default_value<int>::value;
     };
 
 
-    int mlp_impl::load_parameters(const std::string) {
+    int mlp_impl::load_parameters(const std::string s) {
+        std::printf("load_parameters, %s\n", s.c_str());
 
         return default_value<int>::value;
     };
 
-    int mlp_impl::save_parameters(const std::string) const {
+    int mlp_impl::save_parameters(const std::string s) const {
+        std::printf("save_parameters, %s\n", s.c_str());
 
         return default_value<int>::value;
     };
