7dd30796e8b87972511441d09a65f3bb2a767b87
Edouard Levasseur <lewardo23@gmail.com>
2022-06-04 18:04:26 +0100

    trimmed whitespace

--- include/random.h
+++ include/random.h
@@ -11,60 +11,60 @@
 #include "utils.h"
 
 
-constinit double RAND_COEFF = RAND_MAX / 2;
+static constexpr double RAND_COEFF = RAND_MAX / 2;
 
 namespace ml::internal::random {
-    
+
     /*
      *  detail namespace as to not import other namespaces to `namespace network` directly
      */
 
     namespace detail {
-        
+
         /*
          *  using declarations
          */
 
         using namespace types;
-        
-        
+
+
         /*
          *  float-esque atomic generation of random number in range (-1, 1), with once-executing random seed
          */
-         
+
         template <typename T> requires std::convertible_to<T, flt>
         void generate(T& value) {
             [[ maybe_unused ]]
-            static auto seed = []() { 
+            static auto seed = []() {
                 std::srand( std::time(nullptr) );
                 return 0;
             } ();
-            
+
             value = std::rand() / RAND_COEFF - 1.0f;
         };
-        
-        
+
+
         /*
          *  container concept overload to iterate through all containers and meta-containers until atomic float-esque reached
          */
-        
-        template <typename T> requires std::ranges::range<T> && T::value_type
+
+        template <typename T> requires std::ranges::range<T> && requires { typename T::value_type; }
         void generate(T& container) {
             std::for_each(
-                std::execution::par_unseq, 
-                container.begin(), container.end(), 
+                std::execution::par_unseq,
+                container.begin(), container.end(),
                 [&] (T::value_type& i) {
                     generate(i);
                 }
             );
         };
     };
-    
-    
+
+
     /*
      *  export detail symbols to outer namespace
      */
 
     using detail::generate;
-    
+
 };
